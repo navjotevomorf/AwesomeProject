ca430028112305949ab2acfa883252409a8f345d
navjotevomorf <navjot@evomorf.com>
2023-12-22 11:30:23 +0530

    test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello World </Text>
+      <Text>hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
